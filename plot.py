7335c70e96e153827f1db28df18bd6b464ab405a
olveve <olve.vassgard.eid@gmail.com>
2024-12-03 21:06:48 +0100

    Fikset slik at API får tilgang på guess_count verdiene
    Fikset med Ola og Rasmus

--- plot.py
+++ plot.py
@@ -1,16 +1,36 @@
+from pprint import pprint
 import matplotlib.pyplot as plt
+from Spill.api import getResults
+
+
+results = getResults()
+
+pprint(results)
+
+print(type(results))
+
+
+
 forsøk = []
-folk = []
+antall = []
 
-with open("api.py", encoding ="utf-8") as fil:
-    for linje in fil:
-        verdier = linje.strip().split(":")   # .strip fjærner whitespaces / linjeskift.  split(";") viser at man splitter tallene ved tegnet ;
-        forsøk.append((verdier[0]))
-        folk.append(float(verdier[1])) 
+for result in results:
+    print(result)
+    forsøk.append(result["svar"])
+    antall.append(result["antall"])
+    
+print(forsøk, antall)
 
-plt.barh(forsøk, folk, zorder = 2)
+
+"""
+# verdier = linje.strip().split(":")   # .strip fjærner whitespaces / linjeskift.  split(";") viser at man splitter tallene ved tegnet ;
+forsøk.append((verdier[0]))
+antall.append(float(verdier[1])) 
+
+plt.barh(forsøk, antall, zorder = 2)
 plt.xlim(0, 100)
 plt.xticks(rotation = 50, fontsize = 12)
 plt.yticks(rotation = 30, fontsize = 8)
 plt.grid(zorder = 0)
 plt.show()
+"""
