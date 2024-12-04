23906ef22c12b7af45777ca67e04091f79dd0fb9
OlaLunde <ola.berghlunde@gmail.com>
2024-12-04 21:15:41 +0100

    Fjerna kommentarer

--- Spill/api.py
+++ Spill/api.py
@@ -1,23 +1,14 @@
 import requests as req
 
 URL = "https://rasmusweb.no/spm.php"
-GameID = "mastermind"
+GameID = "mastermind4"
 
 def getResults():
     resultat = req.get(URL + "?id=" + GameID)  # , requestOptions)
-    # print(f"Statuskode: {resultat.status_code}")
     data = resultat.json()
-    # print(data)
     return data
 
 def postResult(result):
     data = {"id": GameID , "svar": result}
     resultat = req.post(URL, json=data)
-    # print(f"Statuskode: {resultat.status_code}")
-    # print(resultat.json())
     return resultat
-
-
-# Testing:
-# getResults()
-# postResult(3)
--- Spill/plot.py
+++ Spill/plot.py
@@ -18,17 +18,3 @@ def plot_results():
     plt.ylabel("Antall Spillere")
     plt.title("Antall forsøk brukt av spillere")
     plt.show()
-
-
-"""
-# verdier = linje.strip().split(":")   # .strip fjærner whitespaces / linjeskift.  split(";") viser at man splitter tallene ved tegnet ;
-forsøk.append((verdier[0]))
-antall.append(float(verdier[1])) 
-
-plt.barh(forsøk, folk, zorder = 2)
-plt.xlim(0, 100)
-plt.xticks(rotation = 50, fontsize = 12)
-plt.yticks(rotation = 30, fontsize = 8)
-plt.grid(zorder = 0)
-plt.show()
-"""
