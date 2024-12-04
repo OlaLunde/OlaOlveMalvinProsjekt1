f78be004ddd90146520ebfa02f7e4b0b3da9b339
OlaLunde <ola.berghlunde@gmail.com>
2024-12-04 18:43:25 +0100

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,18 +1,17 @@
-from pprint import pprint
 import matplotlib.pyplot as plt
+from Spill import api
 forsøk = []
 folk = []
 
-with open("api.py", encoding ="utf-8") as fil:
-    for linje in fil:
-        verdier = linje.strip().split(":")   # .strip fjærner whitespaces / linjeskift.  split(";") viser at man splitter tallene ved tegnet ;
+with open("Spill/api.py", encoding ="utf-8") as fil:
+    for data in fil:
+        verdier = data.strip().split(",")  
         forsøk.append((verdier[0]))
         folk.append(float(verdier[1])) 
 
-plt.barh(forsøk, antall, zorder = 2)
+plt.barh(forsøk, folk, zorder = 2)
 plt.xlim(0, 100)
 plt.xticks(rotation = 50, fontsize = 12)
 plt.yticks(rotation = 30, fontsize = 8)
 plt.grid(zorder = 0)
 plt.show()
-"""
